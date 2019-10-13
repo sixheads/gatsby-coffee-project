505847eebec24a6e441b3102f8d0aca92e4aaf60
John Fry <john@sixheads.com>
2019-10-13 12:35:11 +1100

    Add bootstrap css

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,8 +7,8 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
+import "./bootstrap.min.css"
 import "./layout.css"
 
 const Layout = ({ children }) => <>{children}</>
