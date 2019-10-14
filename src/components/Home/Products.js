84f84e71e55ca0ffc599abf44993c84ba865f5dd
John Fry <john@sixheads.com>
2019-10-14 19:57:53 +1100

    Setup products

---
+++ src/components/Home/Products.js
@@ -0,0 +1,49 @@
+import React from "react"
+import Product from "./Product"
+import Title from "../Globals/Title"
+import { StaticQuery, graphql } from "gatsby"
+
+const getProducts = graphql`
+  {
+    products: allContentfulCoffeeProduct {
+      edges {
+        node {
+          id
+          title
+          price
+          image {
+            fluid {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const Products = () => {
+  return (
+    <div>
+      <StaticQuery
+        query={getProducts}
+        render={data => {
+          return (
+            <section className="py-5">
+              <div className="container">
+                <Title title="our products" />
+                <div className="row">
+                  {data.products.edges.map(({ node: product }) => {
+                    return <Product key={product.id} product={product} />
+                  })}
+                </div>
+              </div>
+            </section>
+          )
+        }}
+      />
+    </div>
+  )
+}
+
+export default Products
